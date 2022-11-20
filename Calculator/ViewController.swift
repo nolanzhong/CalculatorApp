ca7749c7b3013f96d28d9db755f0ae6a7a04b7ad
nolanzhong <nolan.zhong@duke.edu>
2022-11-20 16:53:55 -0500

    created addition function

--- Calculator/ViewController.swift
+++ Calculator/ViewController.swift
@@ -9,16 +9,37 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    
+    @IBOutlet weak var number1: UITextField!
+    @IBOutlet weak var number2: UITextField!
+    
+    
+    @IBOutlet weak var resultLabel: UILabel!
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
-
-    @IBAction func input1(_ sender: Any) {
+    
+    
+    @IBAction func addition(_ sender: Any) {
+        let firstNumber = Int(number1.text!)!
+        let secondNumber = Int(number2.text!)!
+        
+        // first ! to tell that the text is definitely going to be there
+        // second ! to tell that it will definitely be an integer
+        let result = firstNumber + secondNumber
+        resultLabel.text = String(result)
+    }
+    
+    @IBAction func subtract(_ sender: Any) {
+        
         
     }
     
-    @IBAction func input2(_ sender: Any) {
+    @IBAction func divide(_ sender: Any) {
+    }
+    
+    @IBAction func multiply(_ sender: Any) {
     }
     
 }
