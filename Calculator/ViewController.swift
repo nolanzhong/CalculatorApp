430cb92c855944f8ab981da42906fd3d991eabae
nolanzhong <nolan.zhong@duke.edu>
2022-11-20 16:22:00 -0500

    created buttons and layout

--- Calculator/ViewController.swift
+++ Calculator/ViewController.swift
@@ -14,6 +14,12 @@ class ViewController: UIViewController {
         // Do any additional setup after loading the view.
     }
 
-
+    @IBAction func input1(_ sender: Any) {
+        
+    }
+    
+    @IBAction func input2(_ sender: Any) {
+    }
+    
 }
 
