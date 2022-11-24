c218fb97e3f3731f871ead4a5c00d8027094f348
nolanzhong <nolan.zhong@duke.edu>
2022-11-23 23:40:59 -0500

    completed calculator

--- Calculator/ViewController.swift
+++ Calculator/ViewController.swift
@@ -13,7 +13,7 @@ class ViewController: UIViewController {
     @IBOutlet weak var number1: UITextField!
     @IBOutlet weak var number2: UITextField!
     
-    
+    var result = 0
     @IBOutlet weak var resultLabel: UILabel!
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -22,24 +22,46 @@ class ViewController: UIViewController {
     
     
     @IBAction func addition(_ sender: Any) {
-        let firstNumber = Int(number1.text!)!
-        let secondNumber = Int(number2.text!)!
+        if let firstNumber = Int(number1.text!) {
+            if let secondNumber = Int(number2.text!) {
+                result = firstNumber + secondNumber
+                resultLabel.text = String(result)
+            }
+        }
+        
         
         // first ! to tell that the text is definitely going to be there
         // second ! to tell that it will definitely be an integer
-        let result = firstNumber + secondNumber
-        resultLabel.text = String(result)
+        //let result = firstNumber + secondNumber
+        //resultLabel.text = String(result)
     }
     
     @IBAction func subtract(_ sender: Any) {
-        let firstNumber = Int(number1.text!)!
+        if let firstNumber = Int(number1.text!) {
+            if let secondNumber = Int(number2.text!) {
+                result = firstNumber - secondNumber
+                resultLabel.text = String(result)
+            }
+        }
         
     }
     
     @IBAction func divide(_ sender: Any) {
+        if let firstNumber = Int(number1.text!) {
+            if let secondNumber = Int(number2.text!) {
+                result = firstNumber / secondNumber
+                resultLabel.text = String(result)
+            }
+        }
     }
     
     @IBAction func multiply(_ sender: Any) {
+        if let firstNumber = Int(number1.text!) {
+            if let secondNumber = Int(number2.text!) {
+                result = firstNumber * secondNumber
+                resultLabel.text = String(result)
+            }
+        }
     }
     
 }
